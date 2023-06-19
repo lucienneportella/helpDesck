12d61ef61dc35b5eb547fb99a5f5360988de0d16
lucienneportella <giustilucienne@gmail.com>
2023-06-19 06:36:12 -0300

    projeto iniciado

---
+++ src/test/java/com/lucienne/hepdesck/HelpdesckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucienne.hepdesck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdesckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
